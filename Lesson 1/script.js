c7d2f312563a3711ead046481fd3951e5bd6de35
jura441 <102172272+jura441@users.noreply.github.com>
2023-05-12 21:23:54 +0300

    Add files via upload

---
+++ Lesson 1/script.js
@@ -0,0 +1,40 @@
+window.addEventListener("DOMContentLoaded", (event) =>{
+
+//var fname = promt ("Как вас зовут?");
+//var lname = promt ("Как ваша фамилия?");
+
+//firstname.innerHTML = fname;
+//lastname.innerHTML = lname;
+
+//console.log(firstname.innerHTML);
+//console.log(lastname.innerHTML);
+ 
+//var usernumber = prompt("Введите число 1");
+//var usernumber2 = prompt("Введите число 2");
+//usernumber3 = (usernumber*1 + usernumber2*1)/2;
+//console.log(usernumber3);
+
+//var fleshka = prompt("Сколько флешка");
+//var fleshkaFiles = (fleshka*1000) /820;
+//console.log("Это будет" + fleshkaFiles + "файлов на флешке");
+
+//var money = prompt("Сколько денег");
+//var chocoPrice = prompt("Сколько денег стоит шоколадка");
+//var chocolateCount = Math.trunc(money/chocoPrice);
+//var moneyChange = money % chocoPrice;
+//console.log("Это будет" + chocolateCount + "шоколадок останется" + moneyChange + "денег");
+
+//вывести зеркальное отображение числа
+/*var number = 573;
+var first = Math.trunc(number / 100);
+var second = Math.trunc((number % 100) / 10);
+var third = (number % 100) % 10;
+console.log(number + " -> " + third+second+first);*/
+
+//узнать четное или нечетное число
+var number = 573;
+//console.log(number % 2 == 1  ? "нечетное" : "четное");
+
+console.log(['Even', 'Odd'] [number * number % 2]);
+
+});
